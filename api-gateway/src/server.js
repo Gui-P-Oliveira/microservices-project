e5d81e4ffd543b59537f1ef19c8e98ecb56fb2b0
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-10-08 16:04:26 -0300

    refactor: change feedback message of server connection

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -8,4 +8,4 @@ const server = express()
 server.use(express.json())
 server.use(routes)
 
-server.listen(PORT, () => console.log(`Server is listening on PORT:${PORT}`))
+server.listen(PORT, () => console.log(`API-Gateway server is listening on PORT:${PORT}`))
